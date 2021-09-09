ce6b88284cb3e16b30490a8326c7e3f7d0d7ec6e
droid-lover <ersachin20@gmail.com>
2021-09-09 12:50:30 +0530

    Adding up start destination as MainFragment for now

---
+++ app/src/main/java/com/sachin/funfacts/ui/MainFragment.kt
@@ -0,0 +1,17 @@
+package com.sachin.funfacts.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sachin.funfacts.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+
+}
